71fc436d4a422a41f241e441aae0c61d3872fd3f
KevinFrias051 <kevinfrias051@gmail.com>
2023-06-27 12:11:05 -0300

    agregar tarea y eliminar

--- lista/Listado.js
+++ lista/Listado.js
@@ -1,40 +1,66 @@
-const el = document.querySelector("#btnAdd");
+let contador = 0;
+const btnAgregarTarea = document.getElementById("btnAgregarTarea");
+btnAgregarTarea.addEventListener('click', function (e) {
+    contador++;
+    console.log('contador: '+contador);
+})
 
 
-function listaTareas() {
-    let tarea = document.getElementById('form').value;
-    let texto = document.createTextNode(tarea);
-    let nuevaTarea = document.createElement("li");
-    
-    var checkboxmaker = document.createElement('input');
-    checkboxmaker.type = "checkbox";
-    checkboxmaker.id= "id";
-    checkboxmaker.classList = "corrected"; 
-    
-    /* var checkboxmakerDel = document.createElement('input');
-    checkboxmakerDel.type = "checkbox";
-    checkboxmakerDel.id ="eliminar";
-    checkboxmakerDel.classList = "visible";  */
 
-    nuevaTarea.appendChild(checkboxmaker);
-    nuevaTarea.appendChild(texto);
-    
-   /*  nuevaTarea.appendChild(checkboxmakerDel); */
-    
-    document.getElementById("lista").appendChild(nuevaTarea);
+function agregarTarea() {
+    const contenedor = document.getElementById("contenedor");
+    const agregarTitulo = document.getElementById("tituloTarea");
+    const agegarCuerpo = document.getElementById("cuerpoTarea");
 
-}
 
-el.addEventListener("click", listaTareas)
+    const divtarea = document.createElement("div");
+    divtarea.id = 'tarea'+eval(contador);
+    divtarea.className = "tarea verde";//de aca cambio la clase del div
 
-function finalizar(e) {
-    if (e.target.type == "checkbox") {
-        e.target.parentNode.classList.toggle("terminado")
+    const divCuerpo = document.createElement("div");//div del cuer
+    divCuerpo.id = "divCuerpo";
+    const divBotones = document.createElement("div");//div de los botones ELIMINAR Y EDITAR
+    divBotones.id = "divBotones";
+
+    const titulo = document.createElement("p");
+    titulo.id = "idTitulo";
+    titulo.innerText = agregarTitulo.value;
+
+    const textoTarea = document.createElement("p");
+    textoTarea.id = "textoTarea";
+    textoTarea.innerText = agegarCuerpo.value;
+    /* creo el boton para eliminar */
+
+    var btnDel = document.createElement('button');
+    btnDel.id = eval(contador);
+    console.log('btnDel.id:' + eval(contador))
+    btnDel.type = 'button';
+    btnDel.className = "botonEliminar";
+    btnDel.textContent = 'eliminarTarea';
+    btnDel.onclick = function(){
+         const idBoton = obtenerIdBoton(this);
+         eliminarTarea(idBoton);
     }
+
+
+
+
+    divBotones.appendChild(btnDel);
+    divtarea.appendChild(titulo);
+    divtarea.appendChild(divCuerpo);
+    divCuerpo.appendChild(textoTarea);
+    divCuerpo.appendChild(divBotones);
+    contenedor.appendChild(divtarea);
+
 }
-lista.addEventListener("click", function (e) {
-    finalizar(e)
-});
+function obtenerIdBoton(boton) {
+    idBoton = boton.id;
+    console.log('tarea:'+idBoton);
+    return idBoton;
+  }
 
-
-
+function eliminarTarea(idBoton) {
+    const divcontenedor = document.querySelector('.div-contenedor');
+    const tarea = document.querySelector('#tarea'+idBoton);
+    divcontenedor.removeChild(tarea);
+} 
